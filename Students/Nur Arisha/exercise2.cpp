ff3e5686a0341795cb34f6ecfc8d270043e48560
arisha <147675471+ar1sha55@users.noreply.github.com>
2023-10-26 21:12:40 +0800

    3

--- Students/Nur Arisha/exercise2.cpp
+++ Students/Nur Arisha/exercise2.cpp
@@ -3,10 +3,36 @@ using namespace std;
 
 int main()
 {
+    //declaration of variable
     int a = 1;
     double d = 1.0;
-    a = 46.9;
+    //operation i
+    a = 46/9;
+    //display output i
     cout << a;
-    cout<<a;
+    // operation ii
+    a = 46 % 9 + 4 * 4 - 2;
+    //new line
+    cout << "\n";
+    //display output ii
+    cout << a;
+    //operation iii
+    a = 45 + 43 % 5 * ( 23 * 3 % 2);
+    //new line
+    cout << "\n";
+    //display output iii
+    cout << a;
+    //operation iv
+    a %=3 / a + 3;
+    cout << "\n";
+    cout << a;
+    //operation v
+    d += 1.5 * 3 + (++a);
+    cout << "\n";
+    cout << d;
+    //operation vi
+    d -= 1.5 * 3 + a++;
+    cout << "\n";
+    cout << d;
     return 0;
 }
