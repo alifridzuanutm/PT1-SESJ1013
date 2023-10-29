0035c9c289597d2be3d90a7d2604a082604a5e50
arisha <147675471+ar1sha55@users.noreply.github.com>
2023-10-29 21:15:31 +0800

    progress

---
+++ Students/Nur Arisha/assignment.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int calc(int a, int w, int h)
+{
+    int g;
+    double bmr;
+    cout << "Enter gender (M/F): ";
+    cin >> g;
+    //option of gender
+    if (g==M)
+    {
+      bmr =  (10*w) + (6.25*h) - (5*a) + 5;
+    }
+    else
+    {
+        bmr = (10*w) + (6.25*h) - (5*a) - 161;
+    }
+    
+    return bmr;
+
+}
+
+int main()
+{
+    int a;
+    int w;
+    int h;
+
+    cout << "Enter age: ";
+    cin >> a;
+    cout << "Your age is: "<< a;
+
+    while ((a>14) && (a<81))
+    {
+        cout << "\nEnter weight: ";
+        cin >> w;
+        cout << "\nEnter height: ";
+        cin >> h;
+        calc(w, h);
+
+
+
+    }
+    
+
+
+
+    return 0;
+
+}
