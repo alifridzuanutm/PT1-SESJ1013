0238bee7009b59f38cf1a1c5b8e552b4fb02a161
Mahmoud Mustafa Elganzory <148404925+MElganzory@users.noreply.github.com>
2023-11-02 10:02:42 +0800

    Update LabExe2set1.cpp

--- Students/MElganzory/LabExe2set1.cpp
+++ Students/MElganzory/LabExe2set1.cpp
@@ -9,33 +9,25 @@ int main() {
     cin >> number; // Asks the user to enter an integer from the user
 
     int sumOfDigits = 0;
-    int originalNumber = number;
 
     while (number > 0) {
         sumOfDigits = sumOfDigits + (number % 10);
         number = number / 10;
     }
 
-    cout << "The sum of digits for " << originalNumber << " is: " << sumOfDigits << endl;
-
-    // Check if the sum is a multiple of 3, 4, and/or 5
-    bool isMultipleOf3 = (sumOfDigits % 3 == 0);
-    bool isMultipleOf4 = (sumOfDigits % 4 == 0);
-    bool isMultipleOf5 = (sumOfDigits % 5 == 0);
-
     cout << "Sum of digits: " << sumOfDigits << endl;
-    
-    if (isMultipleOf3)
+
+    if (sumOfDigits % 3 == 0){
         cout << "The sum of digits is a multiple of 3." << endl;
-    
-    if (isMultipleOf4)
+    }
+    else if (sumOfDigits % 4 == 0) {
         cout << "The sum of digits is a multiple of 4." << endl;
-    
-    if (isMultipleOf5)
+    }
+    else if (sumOfDigits % 5 == 0){
         cout << "The sum of digits is a multiple of 5." << endl;
-    
-    if (!isMultipleOf3 && !isMultipleOf4 && !isMultipleOf5)
+    }
+    else
         cout << "The sum of digits is not a multiple of 3, 4, or 5." << endl;
-    
+
     return 0;
 }
