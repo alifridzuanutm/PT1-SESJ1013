6fa1a7cd4c28c9b8d11a40a8a831194741c33cbf
lokepeien <147963372+lokepeien@users.noreply.github.com>
2023-11-17 01:17:13 +0800

    4

--- Students/LOKE PEI EN/LE2/SET4.cpp
+++ Students/LOKE PEI EN/LE2/SET4.cpp
@@ -1,48 +1,53 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main(){
 
-int main() {
-    int n;
-    int multiply = 1;
+	int n;
+	int rem,multiply=1;
+	
+	cout<<"Enter an integer number:";
+	cin>>n;
+	
+	do{
+		rem=n%10;
+		cout<<rem;
+		
+		n=n/10;
+		if(n!=0)
+	    cout<<"*";
+		
+		multiply*=rem;
+		
+		
+	}while(n!=0);
 
-    cout << "Enter an integer number: ";
-    cin >> n;
-
-    int m = n;
-
-    cout << "Individual digits: ";
-    
-    do {
-        int rem = m % 10;
-        multiply *= rem;
-       
-        
-        if (m > 9) {
-            cout << rem << " * ";
-        } else {
-            cout << rem << " = "  <<multiply<< endl;
-        }
-         m = m / 10;
-    } while (m >0);
-
-
-if (multiply % 2 == 0) {
-        cout << multiply << " is an even number";}
-        else {
-        cout << multiply << " is an odd number" ;
-    }
-
-    if (multiply % 3 == 0) {
-        cout <<  "& is a multiple of 3" << endl;
-    }
-
-   
-    if (multiply % 5 == 0) {
-        cout << "& is a multiple of 5" << endl;
-    }
-    
-    
-
-    return 0;
+		cout<<"="<<multiply<<endl;
+			
+	
+	
+	if (multiply%2==0){
+	cout<<multiply<<"is even number";
+}
+	
+	else{
+			cout<<multiply<<"is odd number";
 	}
+;
+	
+	if(multiply%3==0){
+		cout<<"& multiples of 3";
+	}
+	else if(multiply%5==0){
+		cout<<"& multiples of 5";
+	}
+	else if(multiply%5==0&&multiply%3==0){
+		cout<<"& multiples of 3 and 5";
+
+		
+	}
+		
+	}
+	
+	
+
 
