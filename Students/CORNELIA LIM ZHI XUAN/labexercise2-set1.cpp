0add988962108980093847e7e03f18cbb0f55aab
CorneliaLim <147963361+CorneliaLim@users.noreply.github.com>
2023-11-19 16:14:38 +0800

    Update labexercise2-set1.cpp

--- Students/CORNELIA LIM ZHI XUAN/labexercise2-set1.cpp
+++ Students/CORNELIA LIM ZHI XUAN/labexercise2-set1.cpp
@@ -1,60 +1,42 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    // Declare variables
-    int number, digit, product = 1;
+    int number;
+    cout << "Enter an integer number: ";
+    cin >> number;
 
-    // Read an integer number from the user
-    std::cout << "Enter an integer number: ";
-    std::cin >> number;
-
-    // Calculate the product of digits
+    int sum = 0;
     int originalNumber = number;
-    std::cout << originalNumber << " = ";
 
-    while (number != 0) {
-        digit = number % 10;
-        product *= digit;
-        std::cout << digit;
-
+    do {
+        int digit = number % 10;
+        sum += digit;
         number /= 10;
-        if (number != 0) {
-            std::cout << " * ";
-        }
+    } while (number != 0);
+
+    cout << "Sum of digits for " << originalNumber << ": ";
+
+    int temp = originalNumber;
+    bool firstDigit = true;
+    while (temp != 0) {
+        int digit = temp % 10;
+        cout << (firstDigit ? "" : " + ") << digit;
+        temp /= 10;
+        firstDigit = false;
     }
 
-    std::cout << " = " << product << std::endl;
+    cout << " = " << sum << endl;
 
-    // Check for multiples of 4, 5, and 7
-    std::cout << product << " is multiples of ";
-    bool isMultiple = false;
-
-    if (product % 4 == 0) {
-        std::cout << "4";
-        isMultiple = true;
+    if (sum % 3 == 0) {
+        cout << sum << " is a multiple of 3." << endl;
     }
-
-    if (product % 5 == 0) {
-        if (isMultiple) {
-            std::cout << " and ";
-        }
-        std::cout << "5";
-        isMultiple = true;
+    if (sum % 4 == 0) {
+        cout << sum << " is a multiple of 4." << endl;
     }
-
-    if (product % 7 == 0) {
-        if (isMultiple) {
-            std::cout << " and ";
-        }
-        std::cout << "7";
+    if (sum % 5 == 0) {
+        cout << sum << " is a multiple of 5." << endl;
     }
-
-    if (!isMultiple) {
-        std::cout << "None";
-    }
-
-    std::cout << std::endl;
 
     return 0;
 }
-
