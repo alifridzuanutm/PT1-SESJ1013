67944f37a53e4bcb34cf983dda7e7526b64b1e1e
Jocelyn Wong Yin Xuan <128278945+Jocelyn0404@users.noreply.github.com>
2023-12-10 14:59:53 +0800

    58

--- Students/Jocelyn/inclassexercise/c5pg57.cpp
+++ Students/Jocelyn/inclassexercise/c5pg57.cpp
@@ -8,30 +8,36 @@ Use {1,2,3,4,5,6} and
  */
 
 int average(int array[], int size){
+	
+	int sum = 0;
+	for (int i = 0 ; i < size ; i++){
+		sum += array[i];
+	}
 
-
-
-    return
+	return sum / size;
 }
 
-double average(int array[], int size){
+double average(double array[], int size){
 
+	double sum = 0;
+	for (int i = 0 ; i < size ; i++){
+		sum += array[i];
+	}
 
-
-    return
+	return sum / size;
 }
 
 int main(){
 	
-    int intArray = {1, 2, 3, 4, 5, 6};
-    int doubleArray = {6.0, 4.4 1.9, 2.9, 3.4, 3.5};
-    int size = sizeof (intArray), sizeof(doubleArray);
+	int intArray[] = {1, 2, 3, 4, 5, 6};
+	double doubleArray[] = {6.0, 4.4, 1.9, 2.9, 3.4, 3.5};
+	int size = sizeof (intArray)/ sizeof(int);
 
-    int intAvg(intArray, size);
-    cout << "The average of int array" << intAvg << endl;
-
-    double doubleAvg(doubleArray, size);
-    cout << "The average of double array" << doubleAvg << endl;
+	int intAvg = average(intArray, size);
+	cout << "The average of int array = " << intAvg << endl;
+    
+	double doubleAvg = average(doubleArray, size);
+	cout << "The average of double array = " << doubleAvg << endl;
 	
 	return 0;
 }
