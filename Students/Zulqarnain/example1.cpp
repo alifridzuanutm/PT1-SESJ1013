662f6d27b99e927dcccf6a69efe8e82a15026496
ZULQARNAIN04 <zulqarnaincyrus@gmail.com>
2024-01-03 12:29:07 +0800

    dsds

---
+++ Students/Zulqarnain/example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int x = 1512;
+    int y = 7634;
+    cout << left;
+    cout << setw(5) << x << setw(7) << y << setw(8) << "Warm"<< endl;
+    cout << right;
+    cout << setw(5) << x << setw(7) << y << setw(8) << "Warm"<< endl;
+    return 0;
+}
